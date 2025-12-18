d061e87434b93108c67d8ec464f98e3dbdd76b80
Anish-Programmer <at7anish@gmail.com>
2025-12-18 12:53:57 +0545

    task 4 completed

---
+++ Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop8.Models
+{
+    internal class Item
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Ingredients { get; set; } = string.Empty;
+        public double Price { get; set; }
+    }
+}
